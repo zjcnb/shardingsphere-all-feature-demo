cda3e499431c8fea4c9cab39165da88f9312c3e0
zhaojinchao <zjcnb0215@gmail.com>
2021-12-23 17:54:12 +0800

    update

--- shardingsphere-readwrite-demo/src/test/java/com/shardingsphere/readwrite/ShardingSphereReadWriteApplicationTest.java
+++ shardingsphere-readwrite-demo/src/test/java/com/shardingsphere/readwrite/ShardingSphereReadWriteApplicationTest.java
@@ -13,11 +13,9 @@ public class ShardingSphereReadWriteApplicationTest {
     @Autowired
     private UserMapper userMapper;
     
-    /** Actual SQL: slave2 ::: select id, name, sharding_key from t_user order by id desc limit 1 **/
     @Test
     public void readTest() {
         User selectUser = new User();
-        selectUser.setId(675402755124736000L);
         selectUser.setShardingKey(1);
         User user = userMapper.select(selectUser);
         System.out.println();
@@ -27,17 +25,11 @@ public class ShardingSphereReadWriteApplicationTest {
         System.out.println();
     }
     
-    /** Actual SQL: master ::: insert into t_user(name, sharding_key) values (?, ?) ::: [master-1, 2] **/
     @Test
     public void writeTest() {
         User user = new User();
-        user.setName("zhaojinchao");
-        user.setShardingKey(1);
+        user.setName("master-a");
+        user.setShardingKey(2);
         userMapper.insert(user);
-        System.out.println();
-        System.out.println();
-        System.out.println("insert id is : " + user.getId());
-        System.out.println();
-        System.out.println();
     }
 }
